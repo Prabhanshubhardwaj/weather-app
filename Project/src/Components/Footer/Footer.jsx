756c5beb5adcde7cde3025e15b47abad2f40318d
PrabhanshuBhardwaj <prabhanshubhardwaj2003@gmail.com>
2025-06-23 23:03:50 +0530

    COMMIT

---
+++ Project/src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>© 2024 Vertex. All rights reserved.</p>
+      <ul>
+        {/* <li><a href="https://www.youtube.com/@GreatStackDev/videos" target='_blank'>Terms of Services</a></li> */}
+        <li>Prabhanshu Bhardwaj</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/apikeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "e218289c5d229bb1b87e4f6c73fc359f",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
